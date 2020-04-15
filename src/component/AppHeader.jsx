fa4be776f7fdaae09c56a32366498c29f4796df7
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-04-15 08:17:04 +0700

    Menambahkan ikon-ikon

--- src/component/AppHeader.jsx
+++ src/component/AppHeader.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import fotoSMA from '../img/fotoSMA3.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Col, Container, Row} from "reactstrap";
+import {Button, Col, Container, Media, Row} from "reactstrap";
 
 class AppHeader extends Component {
     constructor(props) {
@@ -11,7 +11,6 @@ class AppHeader extends Component {
             name: 'Adam Arthur Faizal',
         }
     }
-
     render() {
         return (
             <React.Fragment>
@@ -19,14 +18,56 @@ class AppHeader extends Component {
                     <Container className="themed-container" fluid={true}>
                         <Row>
                             <Col sm="4" className="header-kiri">
-                                <img src={fotoSMA} alt="Arthur" className="rounded-circle"/>
-                                <section>
-                                    <h1>Data Pribadi</h1>
-
-                                </section>
+                                <img src={fotoSMA} alt="Arthur" className="rounded-circle mb-2"/>
+                                <Media>
+                                    <Media left href="#">
+                                        {/*<Media object data-src="holder.js/64x64" alt="Generic placeholder image" />*/}
+                                        <i className="fas fa-address-card fa-3x mr-3"/>
+                                    </Media>
+                                    <Media body>
+                                        <Media heading className="mt-2 mb-3">
+                                            Data Pribadi
+                                        </Media>
+                                        <Media>
+                                            <Media left href="#">
+                                                {/*<Media object data-src="holder.js/64x64" alt="Generic placeholder image" />*/}
+                                                <i className="fas fa-home fa-2x mr-3 mt-3"/>
+                                            </Media>
+                                            <Media body>
+                                                <Media heading className="mt-2">
+                                                    <h6 className="mb-0">Perumahan Josroyo Indah, Jaten, Karanganyar</h6>
+                                                </Media>
+                                            </Media>
+                                        </Media>
+                                        <Media>
+                                            <Media left href="#">
+                                                {/*<Media object data-src="holder.js/64x64" alt="Generic placeholder image" />*/}
+                                                <i className="fas fa-info-circle fa-2x mr-3 mt-3"/>
+                                            </Media>
+                                            <Media body>
+                                                <Media heading className="mt-2">
+                                                    <h6 className="mb-0">Pontianak, 20 Juli 2002</h6>
+                                                    <h6 className="mb-0">Laki-laki</h6>
+                                                    <h6 className="mb-0">Islam</h6>
+                                                </Media>
+                                            </Media>
+                                        </Media>
+                                        <Media>
+                                            <Media left href="#">
+                                                {/*<Media object data-src="holder.js/64x64" alt="Generic placeholder image" />*/}
+                                                <i className="fas fa-phone-square fa-2x mr-3"/>
+                                            </Media>
+                                            <Media body>
+                                                <Media heading className="mt-2">
+                                                    <h6 className="mb-0">+62 812-3453-5633</h6>
+                                                </Media>
+                                            </Media>
+                                        </Media>
+                                    </Media>
+                                </Media>
                             </Col>
                             <Col sm="8" className="header-kanan">
-                                <h1 className="display-3 font-weight-bold">{this.state.name}</h1>
+                                <h1 className="display-4 font-weight-bold">{this.state.name}</h1>
                                 <p>
                                     <Button color="info">Web Developer</Button>{' '}
                                     <Button color="info">Mobile-Apps Developer</Button>{' '}
@@ -83,5 +124,20 @@ Col.propTypes = {
     // see https://github.com/reactstrap/reactstrap/issues/297#issuecomment-273556116
     widths: PropTypes.array,
 }
+
+Media.propTypes = {
+    body: PropTypes.bool,
+    bottom: PropTypes.bool,
+    children: PropTypes.node,
+    className: PropTypes.string,
+    heading: PropTypes.bool,
+    left: PropTypes.bool,
+    list: PropTypes.bool,
+    middle: PropTypes.bool,
+    object: PropTypes.bool,
+    right: PropTypes.bool,
+    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+    top: PropTypes.bool,
+};
 
 export default AppHeader;
