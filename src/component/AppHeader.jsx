4e227c6c4e34372c7bf718840a3b1f4fc90e01ec
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-04-15 10:09:53 +0700

    Akhirnya bagian header selesai juga

--- src/component/AppHeader.jsx
+++ src/component/AppHeader.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import fotoSMA from '../img/fotoSMA3.jpg';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Col, Container, Media, Row} from "reactstrap";
 
 class AppHeader extends Component {
@@ -17,7 +16,7 @@ class AppHeader extends Component {
                 <header className="App-header">
                     <Container className="themed-container" fluid={true}>
                         <Row>
-                            <Col sm="4" className="header-kiri">
+                            <Col sm="5" className="header-kiri d-flex flex-column justify-content-center align-items-center">
                                 <img src={fotoSMA} alt="Arthur" className="rounded-circle mb-2"/>
                                 <Media>
                                     <Media left href="#">
@@ -25,13 +24,13 @@ class AppHeader extends Component {
                                         <i className="fas fa-address-card fa-3x mr-3"/>
                                     </Media>
                                     <Media body>
-                                        <Media heading className="mt-2 mb-3">
-                                            Data Pribadi
+                                        <Media heading className="mt-1 mb-3">
+                                            <h2 className="font-weight-bold">Data Pribadi</h2>
                                         </Media>
                                         <Media>
                                             <Media left href="#">
                                                 {/*<Media object data-src="holder.js/64x64" alt="Generic placeholder image" />*/}
-                                                <i className="fas fa-home fa-2x mr-3 mt-3"/>
+                                                <i className="fas fa-home fa-2x mr-3"/>
                                             </Media>
                                             <Media body>
                                                 <Media heading className="mt-2">
@@ -63,16 +62,46 @@ class AppHeader extends Component {
                                                 </Media>
                                             </Media>
                                         </Media>
+                                        <Media>
+                                            <Media left href="#">
+                                                {/*<Media object data-src="holder.js/64x64" alt="Generic placeholder image" />*/}
+                                                <i className="fas fa-envelope-square fa-2x mr-3"/>
+                                            </Media>
+                                            <Media body>
+                                                <Media heading className="mt-2">
+                                                    <h6 className="mb-0">adam.faizal.af6@gmail.com</h6>
+                                                </Media>
+                                            </Media>
+                                        </Media>
                                     </Media>
                                 </Media>
                             </Col>
-                            <Col sm="8" className="header-kanan">
+                            <Col sm="7" className="header-kanan d-flex flex-column align-items-center justify-content-center">
                                 <h1 className="display-4 font-weight-bold">{this.state.name}</h1>
-                                <p>
+                                <p className="mb-4">
                                     <Button color="info">Web Developer</Button>{' '}
                                     <Button color="info">Mobile-Apps Developer</Button>{' '}
                                     <Button color="info">Cyber Security</Button>
                                 </p>
+                                <Media>
+                                    <Media left href="#">
+                                        {/*<Media object data-src="holder.js/64x64" alt="Generic placeholder image" />*/}
+                                        <i className="fas fa-user-graduate fa-3x mr-3"/>
+                                    </Media>
+                                    <Media body>
+                                        <Media heading className="mt-1 mb-3">
+                                            <h2 className="font-weight-bold">Pendidikan</h2>
+                                        </Media>
+                                        <section>
+                                            <p className="mb-0">2007 - 2009 : <span className="font-weight-bold">SD Negeri 7 Randudongkal, Pemalang</span></p>
+                                            <p className="mb-0">2009 - 2010 : <span className="font-weight-bold">SD Negeri 5 Mangkukusuman, Tegal</span></p>
+                                            <p className="mb-0">2010 - 2013 : <span className="font-weight-bold">SD Negeri 3 Jaten, Karanganyar</span></p>
+                                            <p className="mb-0">2013 - 2016 : <span className="font-weight-bold">SMP Negeri 3 Pedan, Klaten</span></p>
+                                            <p className="mb-0">2016 - 2019 : <span className="font-weight-bold">SMA Negeri 1 Karanganyar, Karanganyar</span></p>
+                                            <p className="mb-0">2019 - Sekarang : <span className="font-weight-bold">Universitas Sebelas Maret, Surakarta</span></p>
+                                        </section>
+                                    </Media>
+                                </Media>
                             </Col>
                         </Row>
                     </Container>
--- src/component/AppMain.jsx
+++ src/component/AppMain.jsx
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 import '../sass/style.css';
+import {Col, Container, Row} from "reactstrap";
 
 class AppMain extends Component {
     render() {
         return (
             <React.Fragment>
                 <main className="App-main">
+                    <Container className="themed-container" fluid={true}>
+                        <Row>
+                            <Col sm="5" className="main-kiri">
 
+                            </Col>
+                            <Col sm="7" className="main-kanan">
+
+                            </Col>
+                        </Row>
+                    </Container>
                 </main>
             </React.Fragment>
         )
