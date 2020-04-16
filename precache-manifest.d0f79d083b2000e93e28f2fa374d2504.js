ec0146b041e564b2b390d9d84b084427bebd967e
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-04-16 14:26:27 +0700

    Updates

---
+++ precache-manifest.d0f79d083b2000e93e28f2fa374d2504.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c30d9c807436155cdb10c3b46377461",
+    "url": "/Curriculum-Vitae/index.html"
+  },
+  {
+    "revision": "a2cf2fff5cd4299eb7a8",
+    "url": "/Curriculum-Vitae/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "16ac6d50b39c046766a9",
+    "url": "/Curriculum-Vitae/static/css/main.ac9b3da6.chunk.css"
+  },
+  {
+    "revision": "a2cf2fff5cd4299eb7a8",
+    "url": "/Curriculum-Vitae/static/js/2.945c2597.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Curriculum-Vitae/static/js/2.945c2597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ac6d50b39c046766a9",
+    "url": "/Curriculum-Vitae/static/js/main.4b2eee05.chunk.js"
+  },
+  {
+    "revision": "22dc6055935ae8218991",
+    "url": "/Curriculum-Vitae/static/js/runtime-main.ba3398d3.js"
+  },
+  {
+    "revision": "8b055f01b73ad5dc771b2428fdecc34e",
+    "url": "/Curriculum-Vitae/static/media/Cuprum-Regular-webfont.8b055f01.woff"
+  },
+  {
+    "revision": "74295694cbd3cf50532fb9f55622c038",
+    "url": "/Curriculum-Vitae/static/media/IndieFlower-webfont.74295694.woff"
+  },
+  {
+    "revision": "c515268a39c4e767a9042d2d8446a4dc",
+    "url": "/Curriculum-Vitae/static/media/fotoSMA3.c515268a.jpg"
+  }
+]);
